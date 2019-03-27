2796cb7b59ac7e4bc8b80a499d89ffbabcb4ef5a
alli959 <alg35@hi.is>
2019-03-27 18:01:26 +0000

    falldown komið

--- game/tetris.js
+++ game/tetris.js
@@ -10,8 +10,16 @@ var gl;
 
 var NumVertices  = 36;
 
+var lineBuffer;
+
+var vPosition;
+
 var points = [];
 var colors = [];
+
+var locColor;
+
+var vColor;
 
 var xAxis = 0;
 var yAxis = 1;
@@ -20,12 +28,22 @@ var zAxis = 2;
 var axis = 0;
 var theta = [ 0, 0, 0 ];
 
-var boxScale = 1;
+var boxScale = 2;
 var xBoxPoints = [-0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3]
 var yBoxPoints = [-0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0,
                  0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
 
 var zBoxPoints = [-0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3];
+
+
+var yBlockPosition = 0.9;
+
+var yBlockFalldown = -0.1;
+
+var yBlockTranslator = 0.0;
+
+
+
 
 var movement = false;     // Do we rotate?
 var spinX = 0;
@@ -35,6 +53,12 @@ var origY;
 
 var thetaLoc;
 
+
+var testLine = [
+    vec3(-0.3,-0.9, 0.0),
+    vec3(0.3, -0.9, 0.0)
+];
+
 window.onload = function init()
 {
     canvas = document.getElementById( "gl-canvas" );
@@ -42,7 +66,9 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
+
     colorCube();
+
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
@@ -54,24 +80,35 @@ window.onload = function init()
     //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+
     
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
 
-    var vColor = gl.getAttribLocation( program, "vColor" );
+    vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
 
+    lineBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, lineBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(testLine), gl.STATIC_DRAW);
+    
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
+
+
+    vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    proLoc = gl.getUniformLocation(program, "projection");
     thetaLoc = gl.getUniformLocation(program, "rotation"); 
+
+    var proj = perspective( 90.0, 1.0, 0.1, 100.0 );
+    gl.uniformMatrix4fv(proLoc, false, flatten(proj));
     
     //event listeners for mouse
     canvas.addEventListener("mousedown", function(e){
@@ -97,6 +134,9 @@ window.onload = function init()
     render();
 }
 
+
+
+
 function colorCube()
 {
     quad( 1, 0, 3, 2 );
@@ -105,6 +145,17 @@ function colorCube()
     quad( 6, 5, 1, 2 );
     quad( 4, 5, 6, 7 );
     quad( 5, 4, 0, 1 );
+
+    //block
+
+    quad(9, 8, 11, 10);
+    quad(10, 11, 15, 14);
+    quad(11, 8, 12, 15);
+    quad(14, 13, 9, 10);
+    quad(12, 13, 14, 15);
+    quad(13, 12, 8, 9);
+
+
 }
 
 function quad(a, b, c, d) 
@@ -117,13 +168,23 @@ function quad(a, b, c, d)
         vec3( -0.3, -0.9, -0.3 ),
         vec3( -0.3,  0.9, -0.3 ),
         vec3(  0.3,  0.9, -0.3 ),
-        vec3(  0.3, -0.9, -0.3 )
+        vec3(  0.3, -0.9, -0.3 ),
+
+
+
+        //blocks
+
+        vec3( -0.3,  0.8,  -0.2 ),
+        vec3( -0.3,  0.9,  -0.2 ),
+        vec3(  0.0,  0.9,  -0.2 ),
+        vec3(  0.0,  0.8,  -0.2 ),
+        vec3( -0.3,  0.8,  -0.3 ),
+        vec3( -0.3,  0.9, -0.3 ),
+        vec3(  0.0,  0.9, -0.3 ),
+        vec3(  0.0,  0.8, -0.3 )
     ];
 
-    var testLine = [
-        vec3(-0.3,-0.9, 0.0),
-        vec3(0.3, -0.9, 0.0)
-    ]
+
 
     var vertexColors = [
         [ 0.0, 0.0, 0.0, 1.0 ],  // black
@@ -134,7 +195,6 @@ function quad(a, b, c, d)
         [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
         [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
         [ 1.0, 1.0, 1.0, 1.0 ],   // white
-        [0.32, 0.32, 0.32, 0.5]
     ];
 
     // We need to parition the quad into two triangles in order for
@@ -145,25 +205,46 @@ function quad(a, b, c, d)
     
     var indices = [ a, b, c, a, c, d ];
 
+    console.log(indices);
+
     for ( var i = 0; i < indices.length; ++i ) {
         points.push( vertices[indices[i]] );
         //colors.push( vertexColors[indices[i]] );
     
-        // for solid colored faces use 
-        console.log(c)
-        colors.push(vertexColors[8]);
+        // for solid colored faces use
+        if(a > 7){
+            colors.push(vertexColors[a-8]);
+        }
+        else{
+
+            colors.push(vertexColors[a]);
+        
+        }
+
         
     }
 
-    console.log(points);
+    console.log(points)
+
+
 }
+
+
+var counter = 0;
+
+var maxCounter = 50;
+
 
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+    /*draw box*/
 
-    var ctm = mat4();
+    
+    
+
+    var ctm = lookAt( vec3(1.2, 1.0, 2.0), vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0) );
 
     ctm = mult(ctm, rotateX(spinX));
     ctm = mult(ctm, rotateY(spinY));
@@ -176,7 +257,40 @@ function render()
 
 
 
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+    gl.drawArrays( gl.TRIANGLES, 0, 36);
+
+
+    gl.enable( gl.DEPTH_TEST);
+    if(counter >= maxCounter ){
+        counter = 0;
+        ctm = mult( ctm, translate( 0.0, yBlockTranslator -= 0.1, 0.0 ) );
+        gl.uniformMatrix4fv(thetaLoc, false, flatten(ctm));
+    }
+    else{
+        counter += 1;
+        ctm = mult( ctm, translate( 0.0, yBlockTranslator, 0.0 ) );
+        gl.uniformMatrix4fv(thetaLoc, false, flatten(ctm));
+
+        gl.drawArrays( gl.TRIANGLES, 36, 36);
+    }
+
+
+
+
+
+    
+
+
+/*
+    gl.bindBuffer(gl.ARRAY_BUFFER, lineBuffer);
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.uniform4fv(vColor, flatten(vec4(0.0, 0.0, 0.0, 1.0)))
+    gl.drawArrays(gl.TRIANGLE_STRIPS, NumVertices, 2);*/
+    
+
+
+
+
     
 
 
