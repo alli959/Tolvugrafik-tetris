1f935d3b83ecaabd861419d2975d90487a470e99
alli959 <alg35@hi.is>
2019-03-29 02:58:09 +0000

    lines

--- game/tetris2.js
+++ game/tetris2.js
@@ -8,13 +8,35 @@
 var canvas;
 var gl;
 
-var NumVertices  = 144;
+var NumVertices  = 216;
+
+
+
+
+var NumBlock = 24;
+
+totalFloor = 0;
 
 var points = [];
 var colors = [];
 
+
+
+
 var vBuffer;
+var blockBuffer;
 var vPosition;
+
+
+var xBoxPoints = [0.3, 0.2, 0.1, 0.0, -0.1, -0.2, -0.3];
+
+var yBoxPoints = [1,0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0,
+    -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -11];
+    
+    
+    var zBoxPoints = [0.3, 0.2, 0.1, 0.0, -0.1, -0.2, -0.3];
+    
+var floor = Array(zBoxPoints.length);
 
 var movement = false;     // Do we rotate?
 var spinX = 0;
@@ -28,105 +50,144 @@ var eyesep = 0.2;
 var proLoc;
 var mvLoc;
 
+var xPos = 3;
+var yPos = 10;
+var zPos = 3;
+
+var counter = 0;
+var maxCounter = 20;
+
+var blockTrans = [];
+
+
+var yBlockTranslator = 0.0;
+
+
+//create Floor
+for(var z = 3; z >= -3; z-=1){
+    floor[z] = Array(yBoxPoints.length);
+    for(var y = 10; y>=-10; y-= 1){
+        floor[z][y] = Array(xBoxPoints.length);
+        for(var x = 3; x>=-3; x-=1){
+            floor[z][y][x] = false;
+
+        }
+    }
+}
+
+console.log(floor);
+
+console.log(floor[0][-1][2]);
+
+
+
+
+function addLines(){
+    vertMaker(-0.3, 0.3, -1.0, 1.0, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.9, 0.9, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.8, 0.8, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.7, 0.7, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.6, 0.6, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.5, 0.5, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.4, 0.4, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.3, 0.3, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.2, 0.2, -0.3, 0.3);
+    vertMaker(-0.3, 0.3, -0.1, 0.1, -0.3, 0.3);
+    vertMaker(-0.3, 0.3,  0.0, 0.0, -0.3, 0.3);
+    
+}
 // the 8 vertices of the cube
 var v = [
-    vec3( -0.3, -1.0,  0.3 ),
-    vec3( -0.3,  1.0,  0.3 ),
-    vec3(  0.3,  1.0,  0.3 ),
-    vec3(  0.3, -1.0,  0.3 ),
-    vec3( -0.3, -1.0, -0.3 ),
-    vec3( -0.3,  1.0, -0.3 ),
-    vec3(  0.3,  1.0, -0.3 ),
-    vec3(  0.3, -1.0, -0.3 ),
+ 
 
-    vec3( -0.3, -0.9,  0.3 ),
-    vec3( -0.3,  0.9,  0.3 ),
-    vec3(  0.3,  0.9,  0.3 ),
-    vec3(  0.3, -0.9,  0.3 ),
-    vec3( -0.3, -0.9, -0.3 ),
-    vec3( -0.3,  0.9, -0.3 ),
-    vec3(  0.3,  0.9, -0.3 ),
-    vec3(  0.3, -0.9, -0.3 ),
-
-    vec3( -0.3, -0.8,  0.3 ),
-    vec3( -0.3,  0.8,  0.3 ),
-    vec3(  0.3,  0.8,  0.3 ),
-    vec3(  0.3, -0.8,  0.3 ),
-    vec3( -0.3, -0.8, -0.3 ),
-    vec3( -0.3,  0.8, -0.3 ),
-    vec3(  0.3,  0.8, -0.3 ),
-    vec3(  0.3, -0.8, -0.3 ),
-
-    vec3( -0.3, -0.7,  0.3 ),
-    vec3( -0.3,  0.7,  0.3 ),
-    vec3(  0.3,  0.7,  0.3 ),
-    vec3(  0.3, -0.7,  0.3 ),
-    vec3( -0.3, -0.7, -0.3 ),
-    vec3( -0.3,  0.7, -0.3 ),
-    vec3(  0.3,  0.7, -0.3 ),
-    vec3(  0.3, -0.7, -0.3 ),
-
-    vec3( -0.3, -0.6,  0.3 ),
-    vec3( -0.3,  0.6,  0.3 ),
-    vec3(  0.3,  0.6,  0.3 ),
-    vec3(  0.3, -0.6,  0.3 ),
-    vec3( -0.3, -0.6, -0.3 ),
-    vec3( -0.3,  0.6, -0.3 ),
-    vec3(  0.3,  0.6, -0.3 ),
-    vec3(  0.3, -0.6, -0.3 ),
-
-    vec3( -0.3, -0.5,  0.3 ),
-    vec3( -0.3,  0.5,  0.3 ),
-    vec3(  0.3,  0.5,  0.3 ),
-    vec3(  0.3, -0.5,  0.3 ),
-    vec3( -0.3, -0.5, -0.3 ),
-    vec3( -0.3,  0.5, -0.3 ),
-    vec3(  0.3,  0.5, -0.3 ),
-    vec3(  0.3, -0.5, -0.3 )
 ];
 
-var lines = [ v[0], v[1], v[1], v[2], v[2], v[3], v[3], v[0],
-              v[4], v[5], v[5], v[6], v[6], v[7], v[7], v[4],
-              v[0], v[4], v[1], v[5], v[2], v[6], v[3], v[7],
-              v[8], v[9], v[9], v[10], v[10], v[11], v[11], v[8],
-              v[12], v[13], v[13], v[14], v[14], v[15], v[15], v[12],
-              v[8], v[12], v[9], v[13], v[10], v[14], v[11], v[15],
-              v[16], v[17], v[17], v[18], v[18], v[19], v[19], v[16],
-              v[20], v[21], v[21], v[22], v[22], v[23], v[23], v[20],
-              v[16], v[20], v[17], v[21], v[18], v[22], v[19], v[23],
-              v[24], v[25], v[25], v[26], v[26], v[27], v[27], v[24],
-              v[28], v[29], v[29], v[30], v[30], v[31], v[31], v[28],
-              v[24], v[28], v[25], v[29], v[26], v[30], v[27], v[31],
-              v[32], v[33], v[33], v[34], v[34], v[35], v[35], v[32],
-              v[36], v[37], v[37], v[38], v[38], v[39], v[39], v[36],
-              v[32], v[36], v[33], v[37], v[34], v[38], v[35], v[39],
-              v[40], v[41], v[41], v[42], v[42], v[43], v[43], v[40],
-              v[44], v[45], v[45], v[46], v[46], v[47], v[47], v[44],
-              v[40], v[44], v[41], v[45], v[42], v[46], v[43], v[47],
-            ];
+function vertMaker(lowX, highX, lowY, highY, lowZ, highZ){
+
+    vert = [
+        vec3(lowX, lowY, highZ),
+        vec3(lowX, highY, highZ),
+        vec3(highX, highY, highZ),
+        vec3(highX, lowY, highZ),
+        vec3(lowX, lowY, lowZ),
+        vec3(lowX, highY, lowZ),
+        vec3(highX, highY, lowZ),
+        vec3(highX, lowY, lowZ),
+    ]
+
+    for(var i = 0; i<vert.length; i++){
+        v.push(vert[i]);
+    }
+
+    magic(v.length-8);
+
+}
+
+
+
+
+function magic(magicNumber){
+    var tempLines = [
+        v[0+magicNumber], v[1+magicNumber], v[1+magicNumber], v[2+magicNumber]
+        , v[2+magicNumber], v[3+magicNumber], v[3+magicNumber], v[0+magicNumber],
+        v[4+magicNumber], v[5+magicNumber], v[5+magicNumber], v[6+magicNumber],
+         v[6+magicNumber], v[7+magicNumber], v[7+magicNumber], v[4+magicNumber],
+        v[0+magicNumber], v[4+magicNumber], v[1+magicNumber], v[5+magicNumber],
+         v[2+magicNumber], v[6+magicNumber], v[3+magicNumber], v[7+magicNumber],
+    ];
+    console.log(tempLines);
+    for(var i = 0; i<24; i++){
+        lines.push(tempLines[i]);
+    }
+}
+
+
+var lines = [];
+            
+
 
 
 window.onload = function init()
 {
+
     canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
-
+    
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
     
     gl.enable(gl.DEPTH_TEST);
-
+    
     //
     //  Load shaders and initialize attribute buffers
     //
+    /*
+    vec3( -0.3,  0.9,  -0.2 ),
+    vec3( -0.3,  1.0,  -0.2 ),
+    vec3(  0.0,  1.0,  -0.2 ),
+    vec3(  0.0,  0.9,  -0.2 ),
+    vec3( -0.3,  0.9,  -0.3 ),
+    vec3( -0.3,  1.0, -0.3 ),
+    vec3(  0.0,  1.0, -0.3 ),
+    vec3(  0.0,  0.9, -0.3 )*/
+
+    
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+    
+    addLines();
+    NumVertices = lines.length;
+    vertMaker(0.0, 0.3, 0.9, 1.0, -0.2, -0.3);
     
     vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(lines), gl.STATIC_DRAW );
+    
+    
+
+
 
     vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
@@ -187,7 +248,11 @@ window.onload = function init()
 
 function render()
 {
+
+    var tempY
+
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.disable( gl.DEPTH_TEST );
 
     // Vinstra auga...
     var mv = lookAt( vec3(0.0-eyesep/2.0, 0.0, zDist),
@@ -196,15 +261,85 @@ function render()
     mv = mult( mv, mult( rotateX(spinX), rotateY(spinY) ) );
 
     // Vinstri mynd er � rau�u...
-    gl.uniform4fv( colorLoc, vec4(1.0, 0.0, 0.0, 1.0) );
+    gl.uniform4fv( colorLoc, vec4(1.0, 0.0, 0.0, 0.3) );
     gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
     gl.drawArrays( gl.LINES, 0, NumVertices );
 
+
+
+    /*TODO boolean breyta eftir því hvernig hann snýr*/
+    
+    for(var i = xPos; i>0; i--){
+        if(floor[zPos][yPos][i] == true){
+            console.log(yBlockTranslator);
+            blockTrans.push(vec3(0.0, yBlockTranslator+0.1, 0.0));
+            yPos = 10;
+            yBlockTranslator = 0.0;
+        break;
+        
+        }
+    }
+
+
+    if(blockTrans.length != 0){
+        for(var i = 0; i<blockTrans.length; i++){
+            gl.uniform4fv(colorLoc, vec4(0.0, 1.0, 0.0, 1.0));
+            block = blockTrans[i];
+            mv = mult(mv, translate(block[0], block[1], block[2]));
+            gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+            gl.drawArrays(gl.LINES, NumVertices, 24);
+            mv = mult(mv, translate(-block[0], -block[1], -block[2]));
+
+            
+        }
+    }
+    
+
+    gl.uniform4fv(colorLoc, vec4(0.0, 0.0, 0.0, 1.0))
+    if(counter >= maxCounter){
+        yPos -= 1;
+        
+        counter = 0;
+        yBlockTranslator = yBlockTranslator - 0.1;
+        if(yBlockTranslator <= -2.0){
+            //TODO boolean fyrir hvernig cubinn snýr
+            for(var i = xPos; i>0; i--){
+                floor[zPos][yPos][i] = true;
+                yPos = 10;
+            }
+            /*TODO*/blockTrans.push(vec3(0.0, -1.9, 0.0));
+            yBlockTranslator = 0.0;
+
+            /*TODO*/
+
+
+
+        }
+        mv = mult(mv, translate(0.0, yBlockTranslator, 0.0));
+        gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+        gl.drawArrays(gl.LINES, NumVertices, 24);
+
+
+    
+    }
+
+    else{
+        counter += 1;
+        mv = mult(mv, translate(0.0, yBlockTranslator, 0.0));
+        gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+        gl.drawArrays(gl.LINES, NumVertices, 24);
+    }
+   /* gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays(gl.LINES, NumVertices+NumBlock, totalFloor);*/
+    
     // H�gra auga...
     mv = lookAt( vec3(0.0+eyesep/2.0, 0.0, zDist),
                       vec3(0.0, 0.0, zDist+2.0),
                       vec3(0.0, 1.0, 0.0) );
     mv = mult( mv, mult( rotateX(spinX), rotateY(spinY) ) );
+
+
+
 
     requestAnimFrame( render );
 }
